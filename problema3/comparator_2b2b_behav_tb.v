912ce7a3b2b91ef264ce29e605849c59fe01dda6
prober <grover.ugarte@utec.edu.pe>
2021-09-20 18:58:48 -0500

    behav

--- problema3/comparator_2b2b_behav_tb.v
+++ problema3/comparator_2b2b_behav_tb.v
@@ -1,9 +1,8 @@
-`timescale 1ns/1ns
-module comparator_2b2b_struc_tb;
+module comparator_2b2b_behav_tb;
     reg A, B, C, D;
     wire F1, F2, F3;
 
-    comparator_2b2b_struc comparatorTest (.A(A), .B(B), .C(C), .D(D), .F1(F1), .F2(F2), .F3(F3));
+    comparator_2b2b_behav comparatorTest (.A(A), .B(B), .C(C), .D(D), .F1(F1), .F2(F2), .F3(F3));
     initial begin
         A=0;
         B=1;
@@ -14,7 +13,7 @@ module comparator_2b2b_struc_tb;
         $display("AB: %b", A, "%b\t", B, "CD: %b", C, "%b\t", D, "F1: %b\t", F1,"F2: %b\t", F2,"F3: %b", F3);
     end
     initial begin
-        $dumpfile("comparator_2b2b_struc.vcd");
+        $dumpfile("comparator_2b2b_behav.vcd");
         $dumpvars;
     end
 endmodule
