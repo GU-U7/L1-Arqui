da97cdd2322f74a6175a9d1aad2d3b947cb7188c
GU-U7 <71157869+GU-U7@users.noreply.github.com>
2021-09-23 20:46:20 -0500

    Delete problema3 directory

--- problema3/comparator_2b2b_behav.v
+++
@@ -1,7 +0,0 @@
-module comparator_2b2b_behav (
-    input A, input B, input C, input D, output F1, output F2, output F3
-);
-    assign F1 = ~A&~B&~C&~D | ~A&B&~C&D | A&B&C&D | A&~B&C&~D;
-    assign F2 = ~A&C | ~A&~B&D | ~B&C&D;
-    assign F3 = ~C&A | A&B&~D | B&~C&~D;
-endmodule
